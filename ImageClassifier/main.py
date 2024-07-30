e8d3973f7ef452b63ce4a1ea4c0ec4f7ff7d2e26
luckylukii <lukas.gruenzweil@liwest.at>
2024-07-30 16:13:32 +0200

    Add training functionality

--- ImageClassifier/main.py
+++ ImageClassifier/main.py
@@ -1,9 +1,10 @@
 # Import dependencies
-from torch import nn, save
+from torch import nn, save, load
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+from torchvision.transforms import Resize, ToTensor
+from PIL import Image
 
 train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())
 dataset = DataLoader(train, 32)
@@ -21,10 +22,11 @@ class ImageClassifier(nn.Module):
             nn.Conv2d(64, 64, (3, 3)),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(64*(28-6)*(28-6), 10)
+            nn.Linear(64 * 22 * 22, 10)
         )
 
     def forward(self, x):
+        # print(f"Input shape: {x.shape}")
         return self.model(x)
 
 
@@ -34,8 +36,9 @@ clf = ImageClassifier().to(run_type)
 opt = Adam(clf.parameters(), lr=1e-3)
 loss_fn = nn.CrossEntropyLoss()
 
+
 # Training flow
-if __name__ == "__main__":
+def start_train():
     for epoch in range(int(input("Enter training epochs: "))):
         for batch in dataset:
             x, y = batch
@@ -53,3 +56,33 @@ if __name__ == "__main__":
 
     with open("model_state.pt", "wb") as f:
         save(clf.state_dict(), f)
+
+
+def load_model_state(model, path):
+    model.load_state_dict(load(path))
+    model.eval()
+
+
+def run():
+    # Load model state
+    model_path = "model_state.pt"
+    load_model_state(clf, model_path)
+
+    # Evaluate image
+    img_path = input("Enter image path: ")
+    img = Image.open(img_path).convert("L")
+    img = Resize((28, 28))(img)
+    img_tensor = ToTensor()(img).unsqueeze(0).to(run_type)
+    logits = clf(img_tensor)
+    pred_prob = nn.Softmax(dim=1)(logits)
+    y_pred = pred_prob.argmax(1)
+
+    print(f"Predicted: {y_pred}")
+
+
+if __name__ == "__main__":
+    inp = input("train or test? ").lower()
+    if inp == "train":
+        start_train()
+    elif inp == "test":
+        run()
